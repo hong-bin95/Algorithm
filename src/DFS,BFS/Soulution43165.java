e29db7e0218bb1db44f91004f8876ff50c396e62
Parkhongbin <dev.bin95@gmail.com>
2023-08-25 21:03:24 +0900

    20230825

---
+++ src/DFS,BFS/Soulution43165.java
@@ -0,0 +1,31 @@
+package DFS,BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    
+    int sum = 0;
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        go(numbers, 0, target, 0);
+        answer = sum;
+        return answer;
+    }
+    
+    public void go(int[] numbers, int depth, int target, int result){
+        if( depth == numbers.length ){
+            if(target == result){
+                sum ++;
+            }
+            return;
+        }
+        
+        int a = result + numbers[depth];
+        int b = result - numbers[depth];
+        
+        go(numbers, depth + 1, target, a);
+        go(numbers, depth + 1, target, b);
+    }
+}
